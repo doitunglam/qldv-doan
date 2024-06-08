001d9678041c993732a19253ca9edb98faecbca6
Doi Lam <doitunglam1@gmail.com>
2024-06-08 13:26:11 +0700

    change cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,8 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('run:cron')->everyMinute();
-        //$schedule->command('run:cron')->dailyAt("00:00");
+        // $schedule->command('run:cron')->everyMinute();
+        $schedule->command('run:cron')->dailyAt("00:00");
     }
 
     /**
